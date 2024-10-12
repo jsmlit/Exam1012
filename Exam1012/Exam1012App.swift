ca2c6347320fbccdba61dcfc5e7adee8916c5de0
js <jusemacode@gmail.com>
2024-10-12 09:38:20 +0700

    Initial Commit

---
+++ Exam1012/Exam1012App.swift
@@ -0,0 +1,17 @@
+//
+//  Exam1012App.swift
+//  Exam1012
+//
+//  Created by user on 12.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct Exam1012App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Exam1012/ViewModel/UserViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserViewModel.swift
+//  Exam1012
+//
+//  Created by user on 12.10.2024.
+//
+
+import Foundation
